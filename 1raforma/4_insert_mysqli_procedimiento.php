4df71e87d9d136d285115e40f0e8312c17cba3e2
claudiamarisol <128396290+claudiamarisol@users.noreply.github.com>
2023-04-22 22:10:58 -0400

    se subio practica2

---
+++ 1raforma/4_insert_mysqli_procedimiento.php
@@ -0,0 +1,23 @@
+<?php
+// 4_insert_mysqli_procedimiento.php
+include "../cabecera.php";
+echo $titulo[4];
+$hostname = "localhost";
+$usuario = "root";
+$password = "";
+$nombreBD = "procedimiento_php";
+//Crear conexión
+$conn = mysqli_connect($hostname, $usuario, $password, $nombreBD);
+//Verifica la conexión
+if (!$conn) {
+die("La conexión falló: " . mysqli_connect_error());
+}
+$sql = "INSERT INTO Personas (nombre, apellido, email)
+VALUES ('Alejandro', 'Apaza', 'ale@hotmail.com')";
+if (mysqli_query($conn, $sql)) {
+echo "Nuevo registro creado con éxito";
+} else {
+echo "Error: ".$sql."<br>".mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
